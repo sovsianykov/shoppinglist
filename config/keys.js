e7d2526b2fa3973120cf0c0b26c55368e4c764f4
”sovsianykov” <swebproger@gmail.com>
2021-09-29 17:09:23 +0300

    shopping1

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://user:user1979@cluster0.5ceb9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items')
+
+const app = express();
+
+//BodyParser MiddleWare
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect  to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB is connected...'))
+    .catch(err => console.log(err));
+const port = process.env.PORT || 9000;
+
+app.use('/api/items', items)
+
+app.listen(port,() => console.log(`Server is running on port ${port}`));
+
+
